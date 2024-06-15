6b2b1d3e6bbd17b2e6edd2e51302b537ffab4f8d
Nirmith Victor D'Almeida <63338983+NirmithDev@users.noreply.github.com>
2024-06-15 16:35:40 -0400

    add sign in functionality for users

--- Food-Ordering-System/src/main/java/com/nirmithdev/controller/AuthController.java
+++ Food-Ordering-System/src/main/java/com/nirmithdev/controller/AuthController.java
@@ -2,21 +2,31 @@ package com.nirmithdev.controller;
 
 import com.nirmithdev.config.JwtProvider;
 import com.nirmithdev.model.Cart;
+import com.nirmithdev.model.USER_ROLE;
 import com.nirmithdev.model.User;
 import com.nirmithdev.repository.CartRepository;
 import com.nirmithdev.repository.UserRepository;
+import com.nirmithdev.request.LoginRequest;
 import com.nirmithdev.response.AuthResponse;
 import com.nirmithdev.service.CustomerUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/auth")
@@ -32,6 +42,7 @@ public class AuthController {
     @Autowired
     private CartRepository cartRepository;
     //registration
+    @PostMapping("/signup")
     public ResponseEntity<AuthResponse> createUserHandler(@RequestBody User user) throws Exception{
         User isEmail = userRepository.findByEmail(user.getEmail());
         if(isEmail!=null){
@@ -55,5 +66,36 @@ public class AuthController {
         authResponse.setMessage("Registration Success");
         authResponse.setRole(savedUser.getRole());
         return new ResponseEntity<>(authResponse, HttpStatus.CREATED);
+    }
+    @PostMapping("/signin")
+    public ResponseEntity<AuthResponse> signin(@RequestBody LoginRequest req) throws Exception{
+        String username = req.getEmail();
+        String password = req.getPassword();
+        Authentication authentication = authenicate(username,password);
+        //now we generate token
+        String jwt = jwtProvider.generateToken(authentication);
+        //we want to get role and set authResponse
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        String role=authorities.isEmpty()?null:authorities.iterator().next().getAuthority();
+
+        AuthResponse authResponse = new AuthResponse();
+        authResponse.setJwt(jwt);
+        authResponse.setMessage("Login Success");
+        authResponse.setRole(USER_ROLE.valueOf(role));
+
+        return new ResponseEntity<>(authResponse, HttpStatus.OK);
+    }
+
+    private Authentication authenicate(String username, String password) throws Exception{
+        UserDetails userDetails = customerUserDetailsService.loadUserByUsername(username);
+        if(userDetails==null){
+            throw new BadCredentialsException("Invalid Username.....");
+        }
+        //check if password is in db
+        if(!passwordEncoder.matches(password,userDetails.getPassword())){
+            throw new BadCredentialsException("Invalid Password.............");
+        }
+        //if all match we return authentication
+        return new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
     }
 }
