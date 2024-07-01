54b31f1a7a621c41c3c1cfae7e5626d6e33ed923
Nirmith Victor D'Almeida <63338983+NirmithDev@users.noreply.github.com>
2024-07-01 19:31:58 -0400

    Add files via upload

---
+++ Food-Ordering-System/service/RestaurantServiceImp.java
@@ -0,0 +1,119 @@
+package com.nirmithdev.service;
+
+import com.nirmithdev.dto.RestaurantDto;
+import com.nirmithdev.model.Address;
+import com.nirmithdev.model.Restaurant;
+import com.nirmithdev.model.User;
+import com.nirmithdev.repository.AddressRepository;
+import com.nirmithdev.repository.RestaurantRepository;
+import com.nirmithdev.repository.UserRepository;
+import com.nirmithdev.request.CreateRestaurantRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RestaurantServiceImp implements RestaurantService{
+    @Autowired
+    private RestaurantRepository restaurantRepository;
+    @Autowired
+    private AddressRepository addressRepository;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public Restaurant createRestaurant(CreateRestaurantRequest req, User user) {
+        Address address=addressRepository.save(req.getAddress());
+
+        Restaurant restaurant=new Restaurant();
+        restaurant.setAddress(address);
+        restaurant.setContactInformation(req.getContactInformation());
+        restaurant.setCuisineType(req.getCuisineType());
+        restaurant.setDescription(req.getDescription());
+        restaurant.setImages(req.getImage());
+        restaurant.setOpeningHours(req.getOpeningHours());
+        restaurant.setRegistrationDate(LocalDateTime.now());
+        restaurant.setRegistrationDate(LocalDateTime.now());
+        restaurant.setOwner(user);
+        return restaurantRepository.save(restaurant);
+    }
+
+    @Override
+    public Restaurant updateRestaurant(Long restaurantId, CreateRestaurantRequest updatedRestaurant) throws Exception {
+        Restaurant restaurant = findRestaurantById(restaurantId);
+        if(restaurant.getCuisineType()!=null){
+            restaurant.setCuisineType(updatedRestaurant.getCuisineType());
+        }
+        if(restaurant.getDescription()!=null){
+            restaurant.setDescription(updatedRestaurant.getDescription());
+        }
+        if(restaurant.getName()!=null){
+            restaurant.setName(updatedRestaurant.getName());
+        }
+        return restaurantRepository.save(restaurant);
+    }
+
+    @Override
+    public void deleteRestaurant(Long restaurantId) throws Exception {
+        Restaurant restaurant = findRestaurantById(restaurantId);
+        if(restaurant!=null){
+            restaurantRepository.delete(restaurant);
+        }
+    }
+
+    @Override
+    public List<Restaurant> getAllRestaurants() {
+        return restaurantRepository.findAll();
+    }
+
+    @Override
+    public List<Restaurant> searchRestaurant(String keyWord) {
+        return restaurantRepository.findBySearchQuery(keyWord);
+    }
+
+    @Override
+    public Restaurant findRestaurantById(Long id) throws Exception {
+        Optional<Restaurant> opt = restaurantRepository.findById(id);
+        if(opt.isEmpty()){
+            throw new Exception("restaurant not found with id"+id);
+        }
+        return opt.get();
+    }
+
+    @Override
+    public Restaurant getRestaurantByUserId(Long userId) throws Exception {
+        Restaurant restaurant=restaurantRepository.findByOwnerId(userId);
+        if(restaurant==null){
+            throw new Exception("restaurant not found with user id"+userId);
+        }
+        return restaurant;
+    }
+
+    @Override
+    public RestaurantDto addToFavorites(Long restaurantId, User user) throws Exception {
+        Restaurant restaurant=findRestaurantById(restaurantId);
+        RestaurantDto dto=new RestaurantDto();
+        dto.setDescription(restaurant.getDescription());
+        dto.setImages(restaurant.getImages());
+        dto.setTitle(restaurant.getName());
+        dto.setId(restaurantId);
+        if(user.getFavorites().contains(dto)){
+            user.getFavorites().remove(dto);
+        }else{
+            user.getFavorites().add(dto);
+        }
+        userRepository.save(user);
+        return dto;
+    }
+
+    @Override
+    public Restaurant updateRestaurantStatus(Long id) throws Exception {
+        Restaurant restaurant = findRestaurantById(id);
+        restaurant.setOpen(!restaurant.isOpen());
+        return restaurantRepository.save(restaurant);
+    }
+}
